f6986391090c6b55e98beb434637a82b6eb872f6
JLTSF <locio_2013@hotmail.com>
2023-10-22 13:13:41 -0300

    feat: add clients handler test

--- tests/unit-test/handlers/clients-handler.test.ts
+++ tests/unit-test/handlers/clients-handler.test.ts
@@ -4,6 +4,10 @@ import { mock, mockDeep } from 'jest-mock-extended';
 import { ClientService } from '../../../src/services';
 import { Response, Request } from 'express';
 import { mockBody } from '../utils/request-model-mock';
+import {
+  ClientAlreadyExistsException,
+  QueryLimitExceedsException
+} from '../../../src/helpers/client-service-logs';
 
 const mockResponse = () => {
   const statusResponse = mockDeep<Response>();
@@ -39,12 +43,87 @@ const makeSut = () => {
 };
 
 describe('ClientsHandler', () => {
-  test('Should return a user if create is success', async () => {
-    const { sut, serviceMock, fakeReq, fakeRes } = makeSut();
+  describe('handle', () => {
+    test('Should return a user if create is success', async () => {
+      const { sut, serviceMock, fakeReq, fakeRes } = makeSut();
 
-    await sut.handle(fakeReq, fakeRes);
+      await sut.handle(fakeReq, fakeRes);
 
-    expect(fakeRes.status).toBeCalledWith(200);
-    expect(serviceMock.addClient).toHaveBeenCalled();
+      expect(fakeRes.status).toBeCalledWith(200);
+      expect(serviceMock.addClient).toHaveBeenCalled();
+    });
+
+    test('Should log if throw a error from clientService', async () => {
+      const { sut, serviceMock, fakeReq, fakeRes, loggerMock } = makeSut();
+
+      serviceMock.addClient.mockRejectedValueOnce(new Error());
+
+      await sut.handle(fakeReq, fakeRes);
+
+      expect(fakeRes.status).not.toBeCalledWith(200);
+      expect(loggerMock.error).toHaveBeenCalled();
+    });
+
+    test('Should log and set Status 400 if throw a error ClientAlreadyExistsException', async () => {
+      const { sut, serviceMock, fakeReq, fakeRes, loggerMock } = makeSut();
+
+      serviceMock.addClient.mockRejectedValueOnce(
+        new ClientAlreadyExistsException()
+      );
+
+      await sut.handle(fakeReq, fakeRes);
+
+      expect(fakeRes.status).toBeCalledWith(400);
+      expect(loggerMock.error).toHaveBeenCalled();
+    });
+
+    test('Should log and set Status 500 if instanceof error is different of BaseException', async () => {
+      const { sut, serviceMock, fakeReq, fakeRes, loggerMock } = makeSut();
+
+      serviceMock.addClient.mockRejectedValueOnce(new Error());
+
+      await sut.handle(fakeReq, fakeRes);
+
+      expect(fakeRes.status).toBeCalledWith(500);
+      expect(loggerMock.error).toHaveBeenCalled();
+    });
+  });
+
+  describe('getAll', () => {
+    test('Should return correct result if all params is correct', async () => {
+      const { sut, serviceMock, fakeReq, fakeRes, loggerMock } = makeSut();
+
+      const fakeBody = {
+        count: 1,
+        previus: null,
+        nextPage: null,
+        results: [
+          {
+            id: '65354875ba6541cb0e056607',
+            name: 'jose'
+          }
+        ]
+      };
+      serviceMock.getAll.mockResolvedValueOnce(fakeBody);
+
+      await sut.getAll(fakeReq, fakeRes);
+
+      expect(fakeRes.send).toHaveBeenCalledWith(fakeBody);
+    });
+
+    test('Should log error if limit is more than limitMaxQuery', async () => {
+      const { sut, serviceMock, fakeReq, fakeRes, loggerMock } = makeSut();
+
+      serviceMock.getAll.mockRejectedValueOnce(
+        new QueryLimitExceedsException()
+      );
+
+      await sut.getAll(fakeReq, fakeRes);
+
+      expect(fakeRes.status).toHaveBeenCalledWith(400);
+      expect(fakeRes.status(400).send).toHaveBeenCalledWith({
+        error: 'The limit exceeded the maximum allowed'
+      });
+    });
   });
 });
